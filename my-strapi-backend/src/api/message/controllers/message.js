eabb1743bf78fe535eb89954b13708d5233a8202
Boss <haouamoha33@gmail.com>
2025-04-03 01:37:01 +0100

    going back

--- my-strapi-backend/src/api/message/controllers/message.js
+++ my-strapi-backend/src/api/message/controllers/message.js
@@ -1,71 +1,3 @@
-// "use strict";
-// const { createCoreController } = require("@strapi/strapi").factories;
-// const nodemailer = require("nodemailer");
-
-// module.exports = createCoreController("api::message.message", ({ strapi }) => ({
-//   async create(ctx) {
-//     try {
-//       console.log("Received request:", ctx.request.body); // Debugging
-
-//       // Extract `data` first
-//       const { data } = ctx.request.body;
-//       if (!data) {
-//         console.error("No data received!");
-//         return ctx.throw(400, "Invalid request data");
-//       }
-
-//       // Correctly get values
-//       const { first_name, last_name, email, message } = data;
-//       console.log("Extracted Data:", first_name, last_name, email, message);
-
-//       // Save message to database
-//       console.log("Saving to Strapi:", {
-//         first_name,
-//         last_name,
-//         email,
-//         message,
-//       });
-//       const newMessage = await strapi.entityService.create(
-//         "api::message.message",
-//         {
-//           data: {
-//             FIRST_NAME: first_name,
-//             LAST_NAME: last_name,
-//             EMAIL: email,
-//             MESSAGE: message,
-//           },
-//         }
-//       );
-//       console.log(newMessage)
-//       // Nodemailer Transporter
-//       const transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//           user: process.env.EMAIL_USER,
-//           pass: process.env.EMAIL_PASS,
-//         },
-//       });
-
-//       // Email Options
-//       const mailOptions = {
-//         from: `"${first_name} ${last_name}" <${process.env.EMAIL_USER}>`,
-//         replyTo: email,
-//         to: process.env.RECEIVE_EMAIL,
-//         subject: "New Contact Message",
-//         text: `Name: ${first_name} ${last_name}\nEmail: ${email}\nMessage: ${message}`,
-//       };
-
-//       // Send Email
-//       await transporter.sendMail(mailOptions);
-
-//       return newMessage;
-//     } catch (err) {
-//       console.error("Error sending email:", err);
-//       ctx.throw(500, "Failed to send message");
-//     }
-//   },
-// }));
-
 "use strict";
 const { createCoreController } = require("@strapi/strapi").factories;
 const nodemailer = require("nodemailer");
@@ -75,18 +7,37 @@ module.exports = createCoreController("api::message.message", ({ strapi }) => ({
     try {
       console.log("Received request:", ctx.request.body); // Debugging
 
-      // Vérifier si les données existent
+      // Extract `data` first
       const { data } = ctx.request.body;
       if (!data) {
         console.error("No data received!");
         return ctx.throw(400, "Invalid request data");
       }
 
-      // Extraire les valeurs
+      // Correctly get values
       const { first_name, last_name, email, message } = data;
       console.log("Extracted Data:", first_name, last_name, email, message);
 
-      // Configurer Nodemailer
+      // Save message to database
+      console.log("Saving to Strapi:", {
+        first_name,
+        last_name,
+        email,
+        message,
+      });
+      const newMessage = await strapi.entityService.create(
+        "api::message.message",
+        {
+          data: {
+            FIRST_NAME: first_name,
+            LAST_NAME: last_name,
+            EMAIL: email,
+            MESSAGE: message,
+          },
+        }
+      );
+      console.log(newMessage)
+      // Nodemailer Transporter
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -95,7 +46,7 @@ module.exports = createCoreController("api::message.message", ({ strapi }) => ({
         },
       });
 
-      // Options de l'email
+      // Email Options
       const mailOptions = {
         from: `"${first_name} ${last_name}" <${process.env.EMAIL_USER}>`,
         replyTo: email,
@@ -104,13 +55,14 @@ module.exports = createCoreController("api::message.message", ({ strapi }) => ({
         text: `Name: ${first_name} ${last_name}\nEmail: ${email}\nMessage: ${message}`,
       };
 
-      // Envoyer l'email
+      // Send Email
       await transporter.sendMail(mailOptions);
 
-      return ctx.send({ message: "Email sent successfully!" });
+      return newMessage;
     } catch (err) {
       console.error("Error sending email:", err);
       ctx.throw(500, "Failed to send message");
     }
   },
 }));
+
